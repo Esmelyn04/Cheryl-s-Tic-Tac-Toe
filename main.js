e3745ead554eb1ef3ac79cb97151732da8330f41
Esmelyn04 <EML_JOSE_BAEZ@HOTMAIL.COM>
2024-02-26 21:18:34 -0500

    Refactor  win logic in main.js

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 console.log('Your JS is linked up. Be the person you needed when you were little.')
 
 /*----- constants -----*/
-
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
 
 /*----- app's state (variables) -----*/
 
@@ -40,22 +49,26 @@ function render() {
 
     });
 
-    messages.textContent = `It's ${turn}'s turn!`;
+    messages.textContent = win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
         
 };
 
-function handleTurn(event) {
+function handleTurn() {
     let idx = squares.findIndex(function(square) {
     return square === event.target;
     });
     
     board[idx] = turn;
-    // check your console logs to make sure it's working!
-    //console.log(board);
 
-    turn = turn === 'X' ? 'O' : 'X';
+    win = getWinner();
 
     render();
     };
 
-    win = board[0] && board[0] === board[1] && board[0] === board[2] ? board[0] : null;
+function getWinner() {
+    let winner = null;
+    winningCombos.forEach(function(combo, index) {
+        if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
+    });
+    return winner;
+};
